56cd4821e818c7be72924ef4a82e1c5b5405852c
4lch4 <git@4lch4.email>
2021-10-12 21:30:49 -0500

    feat: added basic testing to the library

---
+++ tests/TestData.ts
@@ -0,0 +1,9 @@
+import { Level } from '../src/interfaces'
+
+export const COLOR_MAP = {
+  [Level.debug]: 'cyan',
+  [Level.info]: 'gray',
+  [Level.warn]: 'yellow',
+  [Level.error]: 'red',
+  [Level.success]: 'green'
+}
