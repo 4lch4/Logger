f8284d9bcf2ec9e8dfb3f775c4f683ee2522f3b5
4lch4 <git@4lch4.email>
2022-06-16 15:32:48 -0500

    feat: added comments to exported class & methods.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import { ILogger, ILoggerOpts, Level } from './interfaces'
+import { ILoggerOpts, Level } from './interfaces'
 import { DefaultColors, DefaultLogFormat, Formatter } from './lib'
 
-export class Logger implements ILogger {
+/**
+ * This class is the main/only class of the library. It contains the various
+ * methods to log messages, and the options provided (if any) are passed to the
+ * formatter class for modifying the output.
+ *
+ * Each method uses the `console` module behind the scenes. This means that you
+ * can provide the ...optionalParams to all methods and they will be passed to
+ * the console module.
+ *
+ * @class Logger
+ */
+export class Logger {
   private formatter: Formatter
 
   constructor(loggerOpts?: ILoggerOpts) {
@@ -14,39 +25,93 @@ export class Logger implements ILogger {
     this.formatter = new Formatter(formatOpt, colorOpts)
   }
 
-  info(msg: string | Object) {
-    console.log(this.formatter.formatMsg(msg, Level.info))
+  /**
+   * Writes the given message to `stdout` with a new line.
+   *
+   * @param msg The content to write to `stdout`.
+   * @param optionalParams Any extra parameters to pass to the console module.
+   */
+  info(msg: string | Object, ...optionalParams: any[]): void {
+    console.log(this.formatter.formatMsg(msg, Level.info), optionalParams)
   }
 
-  warn(msg: string | Object) {
-    console.log(this.formatter.formatMsg(msg, Level.warn))
+  /**
+   * Writes the given message to `stdout` with a new line and colors it yellow
+   * to indicate it is a warning.
+   *
+   * @param msg The content to write to `stdout`.
+   * @param optionalParams Any extra parameters to pass to the console module.
+   */
+  warn(msg: string | Object, ...optionalParams: any[]): void {
+    console.log(this.formatter.formatMsg(msg, Level.warn), optionalParams)
   }
 
-  debug(msg: string | Object) {
-    console.log(this.formatter.formatMsg(msg, Level.debug))
+  /**
+   * Writes the given message to the console at the debug level as long as the
+   * `DEBUG` environment variable is set to `true`.
+   *
+   * @param msg The string/object to write to the debug channel of the console
+   * @param optionalParams Any extra parameters to pass to the console module.
+   */
+  debug(msg: string | Object, ...optionalParams: any[]): void {
+    if (process.env.DEBUG) {
+      console.log(this.formatter.formatMsg(msg, Level.debug), optionalParams)
+    }
   }
 
-  error(msg: string | Error | unknown, ...extra: any[]) {
+  /**
+   * Writes the given content to `stderr`. If the content is an object, it will
+   * be stringified and written to `stderr`. If the content is a string, it will
+   * be written to `stderr` as is. If the content is `unknown`, it will attempt
+   * to be written to `stderr` as an Object.
+   *
+   * Lastly, if there are any optionalParams provided they are joined with a new
+   * line and written to `stderr`.
+   *
+   * @param msg The content to write to `stderr`.
+   * @param optionalParams Any extra parameters to pass to the console module.
+   */
+  error(msg: string | Error | unknown, ...optionalParams: any[]): void {
     if (msg instanceof Error) {
-      console.log(this.formatter.formatMsg(msg.message, Level.error))
+      console.error(this.formatter.formatMsg(msg.message, Level.error))
     } else if (msg instanceof String) {
-      console.log(this.formatter.formatMsg(msg, Level.error))
+      console.error(this.formatter.formatMsg(msg, Level.error))
     } else {
-      console.log(this.formatter.formatMsg(msg as Object, Level.error))
+      console.error(this.formatter.formatMsg(msg as Object, Level.error))
     }
 
-    if (extra.length > 0) {
-      console.log(this.formatter.formatMsg(extra.join('\n'), Level.error))
+    if (optionalParams.length > 0) {
+      console.error(
+        this.formatter.formatMsg(optionalParams.join('\n'), Level.error)
+      )
     }
   }
 
-  success(msg: string) {
-    console.log(this.formatter.formatMsg(msg, Level.success))
+  /**
+   * Writes the given message to `stdout` with a new line and colors it green
+   * to indicate a successful operation.
+   *
+   * @param msg The content to write to `stdout`.
+   * @param optionalParams Any extra parameters to pass to the console module.
+   */
+  success(msg: string, ...optionalParams: any[]): void {
+    console.log(this.formatter.formatMsg(msg, Level.success), optionalParams)
   }
 
-  log(msg: string, level: any) {
-    console.log(this.formatter.formatMsg(msg, level))
+  /**
+   * A convenience method for writing to any level of the console.
+   *
+   * @param msg The content to write.
+   * @param level The level of the message, such as DEBUG, INFO, WARN, ERROR, or SUCCESS.
+   * @param optionalParams Any extra parameters to pass to the console module.
+   */
+  log(msg: string, level: any, ...optionalParams: any[]): void {
+    console.log(this.formatter.formatMsg(msg, level), optionalParams)
   }
 }
 
+/**
+ * A basic instance of the {@link Logger} class using some sane defaults for the
+ * config options.
+ */
 export const logger = new Logger()
