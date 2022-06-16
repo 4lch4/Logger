9f6781dc0829506bfd2d374ca181f67dc22c5c7a
4lch4 <git@4lch4.email>
2022-06-16 17:16:38 -0500

    chore: added some more comments/jsdocs

--- src/lib/Formatter.ts
+++ src/lib/Formatter.ts
@@ -5,53 +5,84 @@ import os from 'os'
 import { IColorOpts, Level, LogFormat } from '../interfaces/index.js'
 import { DefaultDayJSFormats } from './index.js'
 
+/**
+ * A utility class used for formatting messages.
+ *
+ * @class Formatter
+ */
 export class Formatter {
   format: LogFormat
   colors: IColorOpts
 
+  /**
+   * The constructor for the Formatter class. Both parameters are required for
+   * the class to function. The `format` parameter is the format to use for
+   * logging. The `colorOpts` parameter is an object containing the colors to
+   * use for each level.
+   *
+   * @param format The format to use for logging messages.
+   * @param colors An object that defines the colors to use for each level.
+   */
   constructor(format: LogFormat, colors: IColorOpts) {
-    dayjs.extend(AdvancedFormats)
-
     this.colors = colors
     this.format = format
+
+    dayjs.extend(AdvancedFormats)
   }
 
+  /**
+   * Colors a message based on the level of the message.
+   *
+   * @param msg The message to color.
+   * @param level The level of the message.
+   * @returns The given message colored for the given level.
+   */
   colorMsg(msg: string, level: Level) {
     return chalk.keyword(this.colors[level])(msg)
   }
 
-  formatMsg(msg: string | Object, level: Level) {
-    switch (typeof msg) {
-      case 'string': {
-        if (this.format === 'json') {
-          const logOutput = {
-            /** The timestamp for when the message was sent. */
-            time: this.getUTCTime(),
-            host: hostname(),
-            pid: process.pid,
-            level,
-            msg
-          }
+  private stringifyMsg(msg: string | Object): string {
+    if (typeof msg === 'string') return msg
+    return JSON.stringify(msg)
+  }
 
-          return this.colorMsg(JSON.stringify(logOutput), level)
-        } else {
-          const output = [
-            `[${this.getPrettyTime()}]`,
-            `[${level.toUpperCase()}]`,
-            msg
-          ]
-          return this.colorMsg(output.join(' - '), level)
-        }
+  /**
+   * Formats the given message for output based on the `format` parameter that
+   * was provided when instantiating the class and the `level` parameter.
+   *
+   * @param msg The message to format.
+   * @param level The level of the message.
+   * @returns A formatted message to be logged.
+   */
+  formatMsg(msg: string | Object, level: Level) {
+    if (this.format === 'json') {
+      const logOutput = {
+        /** The timestamp for when the message was sent. */
+        time: this.getUTCTime(),
+        host: os.hostname(),
+        pid: process.pid,
+        level,
+        msg: this.stringifyMsg(msg)
       }
-      case 'object':
-        return this.colorMsg(JSON.stringify(msg), level)
+
+      return this.colorMsg(JSON.stringify(logOutput), level)
+    } else {
+      const output = [
+        `[${this.getPrettyTime()}]`,
+        `[${level.toUpperCase()}]`,
+        this.stringifyMsg(msg)
+      ]
+
+      return this.colorMsg(output.join(' - '), level)
     }
   }
 
+  /** Returns the time as UTC Milliseconds. */
   getUTCTime() {
     return dayjs().format(DefaultDayJSFormats.utcMillisecondsFormat)
   }
 
+  /** Returns the current time using a pretty format: `YYYY.MM.DD-HH:mm:ss` */
   getPrettyTime() {
     return dayjs().format(DefaultDayJSFormats.prettyFormat)
   }
--- src/lib/defaults.ts
+++ src/lib/defaults.ts
@@ -1,5 +1,6 @@
 import { IColorOpts, LogFormat } from '../interfaces/index.js'
 
+/** An object containing the default colors to use for each logging level. */
 export const DefaultColors: IColorOpts = {
   debug: 'cyan',
   error: 'red',
@@ -8,9 +9,14 @@ export const DefaultColors: IColorOpts = {
   warn: 'yellow'
 }
 
+/** The default format to use for logging. */
 export const DefaultLogFormat: LogFormat = 'plaintext'
 
+/** The default formatting options to use w/ DayJS. */
 export const DefaultDayJSFormats = {
+  /** The standard pretty format: `YYYY.MM.DD-HH:mm:ss` */
   prettyFormat: 'YYYY.MM.DD-HH:mm:ss',
+
+  /** For formatting the time as UTC Milliseconds. */
   utcMillisecondsFormat: 'x'
 }
