889524fb3822e3eeee1637fc03c10a940055fec7
4lch4 <git@4lch4.email>
2022-06-17 01:24:26 -0500

    refactor: downgraded chalk

--- src/interfaces/IColorOpts.ts
+++ src/interfaces/IColorOpts.ts
@@ -1,13 +1,32 @@
+/*
+ * —————————————————————————————————————————————————————————————————————————————
+ * Project Name			— Logger
+ * Project Version	— 1.8.1
+ * Project Desc.		— An API for easily generating a new project or individual components.
+ * Author						— Devin W. Leaman (4lch4)
+ * Company					— 4lch4 Industries, LLC.
+ * —————————————————————————————————————————————————————————————————————————————
+ * File Path				— /src/interfaces/IColorOpts.ts
+ * File Created			— 2021-10-07 @ 13:12:22-05:00
+ * Last Modified		— 2022-06-17 @ 01:23:56-05:00
+ * Modified By			— Devin W. Leaman (4lch4) (hey@4lch4.email)
+ * —————————————————————————————————————————————————————————————————————————————
+ * MIT License ⸺ http://www.opensource.org/licenses/MIT
+ * 
+ * Copyright (c) 2022, Devin W. Leaman (4lch4) (hey@4lch4.email)
+ * —————————————————————————————————————————————————————————————————————————————
+ */
+
 import { Color } from 'chalk'
 import { Level } from './index.js'
 
 /** The options for the color of each log level. */
 export interface IColorOpts {
-  [Level.debug]: Color
-  [Level.error]: Color
-  [Level.info]: Color
-  [Level.success]: Color
-  [Level.warn]: Color
+  [Level.debug]: typeof Color
+  [Level.error]: typeof Color
+  [Level.info]: typeof Color
+  [Level.success]: typeof Color
+  [Level.warn]: typeof Color
 }
 
 /**
@@ -16,9 +35,9 @@ export interface IColorOpts {
  * default colors, see the exported `DefaultColors` object.
  */
 export interface IColorOptsInput {
-  [Level.debug]?: string | Color
-  [Level.error]?: string | Color
-  [Level.info]?: string | Color
-  [Level.success]?: string | Color
-  [Level.warn]?: string | Color
+  [Level.debug]?: string | typeof Color
+  [Level.error]?: string | typeof Color
+  [Level.info]?: string | typeof Color
+  [Level.success]?: string | typeof Color
+  [Level.warn]?: string | typeof Color
 }
