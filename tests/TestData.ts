e8e281720dc09cb2b3bcc0d92282c2d9d910f3aa
4lch4 <git@4lch4.email>
2023-04-17 20:33:25 -0500

    refactor: removed _all_ testing for now, need a new framework

--- tests/TestData.ts
+++
@@ -1,9 +0,0 @@
-import { Level } from '../dist/interfaces/index.js'
-
-export const COLOR_MAP = {
-  [Level.debug]: 'cyan',
-  [Level.info]: 'gray',
-  [Level.warn]: 'yellow',
-  [Level.error]: 'red',
-  [Level.success]: 'green'
-}
